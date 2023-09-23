cd94d2a94220b6aaf8d75ab26f1c74a070106791
LirionMist <nkljozegov@gmail.com>
2023-09-23 10:25:10 +0500

    Create Practice5.php

---
+++ Practice5.php
@@ -0,0 +1,42 @@
+<?php
+$host = "db4.myarena.ru";      // Адрес сервера базы данных
+$dbname = "u19978_b05";        // Имя базы данных
+$user = "u19978_b05";          // Имя пользователя
+$password = "mD0kT6hL8r";      // Пароль
+
+try {
+    $connection = new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8', $user, $password);
+
+    } catch (PDOException $e) {
+        die("Ошибка подключения: " . $e->getMessage());
+    }
+
+    $result = $connection->query('SELECT * FROM  `product` WHERE 0');
+
+    if ($result){
+    if ($result->rowCount() == 0) {
+        echo "Запрос не вернул результатов.";
+    } else {
+
+        echo "<table border='1'>";
+        // Вывод заголовков таблицы
+        echo "<tr>";
+        for ($i = 0; $i < $result->columnCount(); $i++) {
+            $column = $result->getColumnMeta($i);
+            echo "<th>{$column['name']}</th>";
+        }
+        echo "</tr>";
+        
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            echo "<tr>";
+            foreach ($row as $value) {
+                echo "<td>".$value."</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+        }}
+        else {
+            echo "Запросвыполнен с ошибкой ";
+        }
+?>
