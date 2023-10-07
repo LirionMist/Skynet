5d603ef6357b48433132fe2f6550d7060b56548a
LirionMist <nkljozegov@gmail.com>
2023-10-07 10:12:14 +0500

    Create index.php

---
+++ P8/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sing in</title>
+</head>
+<body>
+    <form action="action.php" method="post">
+        <p>Login <input type="text" name="login" required></p>
+        <p>Password* <input type="password" name="password" required></p>
+        <button>Sing in</button>
+    </form>
+    
+</body>
+</html>
