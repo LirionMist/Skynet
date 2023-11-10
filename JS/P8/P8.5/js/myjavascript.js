e59a68c55a55614f4f31b72c8eeae754a5f03e91
LirionMist <nkljozegov@gmail.com>
2023-11-10 11:47:28 +0500

    небольшое обновление

--- JS/P8/P8.5/js/myjavascript.js
+++ JS/P8/P8.5/js/myjavascript.js
@@ -5,13 +5,13 @@ surname: 'Ожегов',
 name : 'Николай',
 patronymic : 'Алексеевич',
 occupation : 'Директор',
-degree : 'Среднее профессиональное',
+degree : ['Среднее профессиональное',' Высшее',' Дополнительное профессиональное'],
 category : 'Первая',
 education : 'ГБПОУ ПКК ОНИКС',
-discipline : 'Информатика',
+discipline : ['Информатика', 'Математика','Физика'],
 note : '',
 
 };
 
-paragraph.innerHTML = "ФИО " + person.surname + ' ' + person.name + ' ' + person.patronymic +'<br>' + "Должность " + person.occupation +'<br>' + "Уровень образования " + person.degree+'<br>' + "Категория " + person.category +'<br>' + "Образование " + person.education +'<br>' + "Дисциплина " + person.discipline +'<br>' + "Примечание " + person.note ;
+paragraph.innerHTML = '<h2>' + person.surname + ' ' + person.name + ' ' + person.patronymic + '</h2>' + "<i>" + "Должность " + "</i>" + person.occupation +'<br>' + "Уровень образования " + person.degree+'<br>' + "Категория " + person.category +'<br>' + "Образование " + person.education +'<br>' + "Дисциплина " + person.discipline +'<br>' + "Примечание " + person.note ;
 
