8319a26d35f1309e814a1ab3513506138d169e4c
LirionMist <nkljozegov@gmail.com>
2023-11-11 08:57:36 +0500

    задание 8.5

--- JS/P8/P8.5/js/myjavascript.js
+++ JS/P8/P8.5/js/myjavascript.js
@@ -1,17 +1,23 @@
 let paragraph = document.querySelector("#myParagraph");
-let person={
-    
-surname: 'Ожегов',
-name : 'Николай',
-patronymic : 'Алексеевич',
-occupation : 'Директор',
-degree : ['Среднее профессиональное',' Высшее',' Дополнительное профессиональное'],
-category : 'Первая',
-education : 'ГБПОУ ПКК ОНИКС',
-discipline : ['Информатика', 'Математика','Физика'],
-note : '',
+let person = {
+
+    surname: 'Ожегов',
+    name: 'Николай',
+    patronymic: 'Алексеевич',
+    occupation: 'Директор',
+    degree: ['Среднее профессиональное', ' Высшее', ' Дополнительное профессиональное'],
+    category: 'Первая',
+    education: 'ГБПОУ ПКК ОНИКС',
+    discipline: ['Информатика', 'Математика', 'Физика'],
+    note: '',
 
 };
 
-paragraph.innerHTML = '<h2>' + person.surname + ' ' + person.name + ' ' + person.patronymic + '</h2>' + "<i>" + "Должность " + "</i>" + person.occupation +'<br>' + "Уровень образования " + person.degree+'<br>' + "Категория " + person.category +'<br>' + "Образование " + person.education +'<br>' + "Дисциплина " + person.discipline +'<br>' + "Примечание " + person.note ;
+let disciplinePointText;
+for (let index = 0; index < person.degree.length; index++) {
+    disciplinePointText = disciplinePointText + "<li>" + person.degree[index] + "</li>";
+}
 
+paragraph.innerHTML = '<h2>' + person.surname + ' ' + person.name + ' ' + person.patronymic + '</h2>' + "<i>" + "Должность: " + "</i>" + person.occupation + '<br>' + "<i>" + "Уровень образования: " + "</i>" + person.degree + '<br>' + "<i>" + "Категория: " + "</i>" + person.category + '<br>' + "<i>" + "Образование: " + "</i>" + person.education + '<br>' + "<i>" + "Дисциплина: " + "</i>" + "<ul>" + disciplinePointText + "</ul>" + '<br>';
+
+paragraph.innerHTML = paragraph.innerHTML + "<i>" + "Примечание: " + "</i>" + person.note;
