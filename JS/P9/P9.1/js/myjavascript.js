567672ac59db399617ad10fbebc88db704517371
LirionMist <nkljozegov@gmail.com>
2023-11-17 11:32:46 +0500

    практика 9.1

---
+++ JS/P9/P9.1/js/myjavascript.js
@@ -0,0 +1,23 @@
+function addTagsToHtml(Tag, className = 'default') {
+    const element = document.createElement(Tag); 
+    element.classList.add(className);
+    document.body.append(element);
+}
+
+function addTextToHtml(findElementByClass, textElement, tag) {
+    const block = document.querySelector('.' + findElementByClass);
+    const newTag = document.createElement(tag); 
+
+    const textNode = document.createTextNode(textElement); 
+    newTag.appendChild(textNode); 
+
+    block.appendChild(newTag); 
+
+    document.body.appendChild(block); 
+}
+
+
+addTagsToHtml('main', 'content'); 
+
+addTextToHtml('content','Практика 9', 'h1');
+
